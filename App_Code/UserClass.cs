66484e27ee39203cd46ba05ee0158fd37e0bf1f9
Harsh Bhakt <harshbbhakta@gmail.com>
2014-03-18 15:07:28 +0530

    class files commented for GUI guys

--- App_Code/UserClass.cs
+++ App_Code/UserClass.cs
@@ -1,103 +1,103 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Web;
 
 
-public class UserClass : ConnectionClass
-{
-    public class StudentClass : ConnectionClass
-    {
-        #region "All Field"
+//public class UserClass : ConnectionClass
+//{
+//    public class StudentClass : ConnectionClass
+//    {
+//        #region "All Field"
 
-        private int _userid, _teamid;
-        private DateTime user_createddate;
-        private string _username, _userpassword;
-        private char _userisactive;
+//        private int _userid, _teamid;
+//        private DateTime user_createddate;
+//        private string _username, _userpassword;
+//        private char _userisactive;
 
-        #endregion
-
-
-        #region "All Properties"
+//        #endregion
 
 
-        public int User_Id
-        {
-            get
-            {
-                return _userid;
-            }
-            set
-            {
-                _userid = value;
-            }
-        }
+//        #region "All Properties"
 
 
-        public int Team_Id
-        {
-            get
-            {
-                return _teamid;
-            }
-            set
-            {
-                _teamid = value;
-            }
-        }
+//        public int User_Id
+//        {
+//            get
+//            {
+//                return _userid;
+//            }
+//            set
+//            {
+//                _userid = value;
+//            }
+//        }
 
 
-        public DateTime User_CreatedDate
-        {
-            get
-            {
-                return user_createddate;
-            }
-            set
-            {
-                user_createddate = value;
-            }
-        }
-        public string User_Name
-        {
-            get
-            {
-                return _username;
-            }
-            set
-            {
-                _username = value;
-            }
-        }
-
-        public string User_Password
-        {
-            get
-            {
-                return _userpassword;
-            }
-            set
-            {
-                _userpassword = value;
-            }
-        }
+//        public int Team_Id
+//        {
+//            get
+//            {
+//                return _teamid;
+//            }
+//            set
+//            {
+//                _teamid = value;
+//            }
+//        }
 
 
-        public char User_IsActive
-        {
-            get
-            {
-                return _userisactive;
-            }
-            set
-            {
-                _userisactive = value;
-            }
-        }
+//        public DateTime User_CreatedDate
+//        {
+//            get
+//            {
+//                return user_createddate;
+//            }
+//            set
+//            {
+//                user_createddate = value;
+//            }
+//        }
+//        public string User_Name
+//        {
+//            get
+//            {
+//                return _username;
+//            }
+//            set
+//            {
+//                _username = value;
+//            }
+//        }
+
+//        public string User_Password
+//        {
+//            get
+//            {
+//                return _userpassword;
+//            }
+//            set
+//            {
+//                _userpassword = value;
+//            }
+//        }
 
 
-        #endregion////
+//        public char User_IsActive
+//        {
+//            get
+//            {
+//                return _userisactive;
+//            }
+//            set
+//            {
+//                _userisactive = value;
+//            }
+//        }
 
 
-    }
-}
+//        #endregion////
+
+
+//    }
+//}
