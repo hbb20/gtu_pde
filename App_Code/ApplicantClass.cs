164830d8f18fd4d8c7fa2a7eac729bfcd28db22b
rutuhelly <shelat_rutu@yahoo.in>
2014-03-24 12:11:32 +0530

    just commit

--- App_Code/ApplicantClass.cs
+++ App_Code/ApplicantClass.cs
@@ -6,12 +6,12 @@ using System.Data.SqlClient;
 using System.Data;
 
 
-public class InventorClass:ConnectionClass
+public class InventorClass : ConnectionClass
 {
     #region "All Field"
 
     private int _applicantid, _form_id;
-    private string _applicantname, _applicantnationality, _applicantaddress,_applicantmobileno,_applicantemailid;
+    private string _applicantname, _applicantnationality, _applicantaddress, _applicantmobileno, _applicantemailid;
 
     #endregion
 
@@ -107,30 +107,6 @@ public class InventorClass:ConnectionClass
 
     #endregion
 
-    #region "Methods"
-    public bool SaveForm()
-    {
-        SqlParameter[] pmt = new SqlParameter[5];
-
-        pmt[0] = new SqlParameter("@F_Name", this.F_Name);
-        pmt[0].DbType = DbType.String;
-
-        pmt[1] = new SqlParameter("@F_SystemDateTime", this.F_SystemDateTime);
-        pmt[1].DbType = DbType.DateTime;
-
-        pmt[2] = new SqlParameter("@F_UploadDateTime", this.F_UploadDateTime);
-        pmt[2].DbType = DbType.DateTime;
-
-        pmt[3] = new SqlParameter("@F_Path", this.F_Path);
-        pmt[3].DbType = DbType.String;
-
-        pmt[4] = new SqlParameter("@D_Id", this.D_Id);
-        pmt[4].DbType = DbType.String;
-
-
-        DataAccess.ExecuteNonQuery(strConnectionString, CommandType.StoredProcedure, "SaveForm", pmt);
-        return true;
-    }
-
-    #endregion
+   
+    
 }
